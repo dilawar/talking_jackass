0b9999b4d504a2278ea3a428896badb38e2ec212
Dilawar Singh <dilawars@ncbs.res.in>
2018-02-09 12:22:43 +0530

    Added jackass.

---
+++ jackass.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: jackass.sh
+#
+#         USAGE: ./jackass.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
+#  ORGANIZATION: NCBS Bangalore
+#       CREATED: Friday 09 February 2018 10:47:03  IST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+set -e
+
+# record 5 sec into current directory.
+
+OUTFILE=/tmp/rec.wav
+DURATION=10
+NOISE_PROFILE=./noise_slc.prof
+
+while true; do
+    arecord -d $DURATION -t wav $OUTFILE
+    # now remove noise.
+    FILTERED_FILE=$OUTFILE.filtered.wav
+    sox $OUTFILE $FILTERED_FILE noisered $NOISE_PROFILE 0.4
+    echo "Done removing noise -> $FILTERED_FILE"
+    
+    # spectogram
+    STATS=$(sox $FILTERED_FILE -n rate 6k spectrogram stats 2>&1)
+    #echo -e $STATS 
+    #echo -e $STATS | grep -oP "RMS\s+\w+\s+\S+\s+\S+" || echo "failed #to grep"
+    RMS_LEVEL=`echo -e $STATS | grep -oP "RMS lev dB\s+\K\S+"`
+    if [ "$RMS_LEVEL" -lt -30 ]; then
+        notify-send "Too loud.. Trigger jackass."
+    fi
+done
